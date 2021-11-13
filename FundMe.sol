7679dfa0175476e9b164d9fc4b264df6e330d4f6
Steven Adema <stevenadema@gmail.com>
2021-11-12 23:22:29 -0500

    fundme update

--- FundMe.sol
+++ FundMe.sol
@@ -10,10 +10,18 @@ contract FundMe {
     
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
-        // what is ETH/USD rate?
-        
+        // what the ETH USD convsersion rate
     }
     
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
     
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+            (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer);
+    }
     
 }
